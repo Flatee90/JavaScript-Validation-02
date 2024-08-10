eca1f755f327a2d59d8e0fd5c32eadac1746f4d2
Folashade Lateef <flatee90@gmail.com>
2024-08-09 20:05:42 -0500

    fortune assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello from index.js")
+let myName;
+myName = "Shade";
+let lenghtOfName = myName.length;
+if (lenghtOfName < 5) {
+    console.log("You will marry soon!")
+}
+
+myName = "Folashade";
+let lenghtofName = myName.length;
+if (lenghtOfName > 7) {
+    console.log("You will marry late in life!")
+}
+
+myName = "Folasha";
+let lenghtName = myName.length;
+if (lenghtOfName>7) {
+    console.log("You will encounter a once-in-a-lifetime opportunity")
+}
+
+let character = myName[0];
+console.log("first letter is " + character);
+if(character === "F") {
+    console.log("You will be rich!")
+}
+
+if(myName.includes("a")) {
+    console.log("You will meet the love of your life!")
+}
